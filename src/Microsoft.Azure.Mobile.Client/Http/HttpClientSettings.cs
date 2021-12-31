a00b07ab36be9a67e66c0cf2f7335f9695a6c493
Abraham Brull <ABrull@chorus.cloud>
2021-12-31 10:09:18 -0500

    allow configuring request timeout

---
+++ src/Microsoft.Azure.Mobile.Client/Http/HttpClientSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Microsoft.WindowsAzure.MobileServices.Http
+{
+    public class HttpClientSettings
+    {
+        /// <summary>
+        /// Constructor with passing in a timeout
+        /// </summary>
+        /// <param name="timeout">the amount of time before a request times out</param>
+        public HttpClientSettings(TimeSpan? timeout = null)
+        {
+            Timeout = timeout ?? TimeSpan.FromMinutes(1);
+        }
+
+        /// <summary>
+        /// The timeout for all http requests
+        /// </summary>
+        public TimeSpan Timeout { get; }
+    }
+}
